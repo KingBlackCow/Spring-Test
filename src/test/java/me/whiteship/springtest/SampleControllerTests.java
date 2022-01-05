3faa6c61cd1c0968bdd7d33e75bb6c2cb8006b0d
KingBlackCow <sgs1159@naver.com>
2022-01-06 00:07:10 +0900

    TestRestTemplate 적용기

--- src/test/java/me/whiteship/springtest/SampleControllerTests.java
+++ src/test/java/me/whiteship/springtest/SampleControllerTests.java
@@ -5,23 +5,29 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 class SampleControllerTests {
 
     @Autowired
     MockMvc mockMvc;
+
+    @Autowired
+    TestRestTemplate testRestTemplate;
 
     @Test
     public void hello() throws Exception {
@@ -31,6 +37,12 @@ class SampleControllerTests {
                 .andExpect(status().isOk())
                 .andExpect(content().string("helloJS"))
                 .andDo(print());
+    }
+
+    @Test
+    public void helloTestRestTemPlate(){
+        String result = testRestTemplate.getForObject("/hello", String.class);
+        assertThat(result).isEqualTo("helloJS");
     }
 
 
